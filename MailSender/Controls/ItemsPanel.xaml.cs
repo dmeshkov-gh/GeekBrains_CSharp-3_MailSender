6baf90cd9ca81a5779155f2717a7dbe8a89ced31
Dmitry_Meshkov <dm_91@bk.ru>
2021-02-20 13:00:16 +0300

    DependencyProperty Title

--- MailSender/Controls/ItemsPanel.xaml.cs
+++ MailSender/Controls/ItemsPanel.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Controls;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MailSender.Controls
 {
@@ -20,6 +8,17 @@ namespace MailSender.Controls
     /// </summary>
     public partial class ItemsPanel : UserControl
     {
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(string)));
+        public string Title 
+        { 
+            get => (string)GetValue(TitleProperty); 
+            set => SetValue(TitleProperty, value); 
+        }
         public ItemsPanel()
         {
             InitializeComponent();
