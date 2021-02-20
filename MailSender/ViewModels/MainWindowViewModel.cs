da9ed68131a89ff1b8a87a68cd5e549897946546
Dmitry_Meshkov <dm_91@bk.ru>
2021-02-20 15:55:37 +0300

    ViewModel of Main window

---
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MailSender.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Почтовый отправитель";
+        private string _status = "Готов...";
+
+        public string Title { get => _title; set => Set(ref _title, value); }
+
+        public string Status { get => _status; set => Set(ref _status, value); }
+    }
+}
