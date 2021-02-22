3afa001482f04c303531affe116e179edff4100a
Dmitry_Meshkov <dm_91@bk.ru>
2021-02-22 19:37:23 +0300

    Creating dependency properties for ItemsPanel - commands, selectedUser

--- MailSender/Controls/ItemsPanel.xaml.cs
+++ MailSender/Controls/ItemsPanel.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows.Controls;
 using System.Windows;
 using System;
+using System.Windows.Input;
+using MailSender.Models;
+using System.Collections.ObjectModel;
 
 namespace MailSender.Controls
 {
@@ -9,6 +12,11 @@ namespace MailSender.Controls
     /// </summary>
     public partial class ItemsPanel : UserControl
     {
+        public ItemsPanel()
+        {
+            InitializeComponent();
+        }
+
         public static readonly DependencyProperty TitleProperty =
             DependencyProperty.Register(
                 nameof(Title),
@@ -18,7 +26,6 @@ namespace MailSender.Controls
 
         private static void OnTitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            throw new NotImplementedException();
         }
 
         public string Title 
@@ -26,9 +33,70 @@ namespace MailSender.Controls
             get => (string)GetValue(TitleProperty); 
             set => SetValue(TitleProperty, value); 
         }
-        public ItemsPanel()
+
+
+        public static readonly DependencyProperty ServersProperty =
+            DependencyProperty.Register(
+                nameof(Servers),
+                typeof(ObservableCollection<>),
+                typeof(ItemsPanel));
+
+        public ObservableCollection<Server> Servers
         {
-            InitializeComponent();
+            get => (ObservableCollection<Server>)GetValue(SelectedUserProperty);
+            set => SetValue(SelectedUserProperty, value);
         }
+
+
+        public static readonly DependencyProperty SelectedUserProperty =
+            DependencyProperty.Register(
+                nameof(SelectedUser),
+                typeof(User),
+                typeof(ItemsPanel));
+
+        public User SelectedUser
+        {
+            get => (User)GetValue(SelectedUserProperty);
+            set => SetValue(SelectedUserProperty, value);
+        }
+
+
+        public static readonly DependencyProperty AddCommandProperty =
+            DependencyProperty.Register(
+                nameof(AddCommand),
+                typeof(ICommand),
+                typeof(ItemsPanel));
+
+        public ICommand AddCommand
+        {
+            get => (ICommand)GetValue(AddCommandProperty);
+            set => SetValue(AddCommandProperty, value);
+        }
+
+
+        public static readonly DependencyProperty EditCommandProperty =
+            DependencyProperty.Register(
+                nameof(EditCommand),
+                typeof(ICommand),
+                typeof(ItemsPanel));
+
+        public ICommand EditCommand
+        {
+            get => (ICommand)GetValue(EditCommandProperty);
+            set => SetValue(EditCommandProperty, value);
+        }
+
+
+        public static readonly DependencyProperty DeleteCommandProperty =
+            DependencyProperty.Register(
+                nameof(DeleteCommand),
+                typeof(ICommand),
+                typeof(ItemsPanel));
+
+        public ICommand DeleteCommand
+        {
+            get => (ICommand)GetValue(DeleteCommandProperty);
+            set => SetValue(DeleteCommandProperty, value);
+        }   
     }
 }
